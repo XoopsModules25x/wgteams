aa183d5ba5cec20832ee79fcb93f3baa5c2130cf
ggoffy <gottfried.gabor@gmail.com>
2025-06-07 15:44:36 +0200

    last stable version for PHP 7

--- xoops_version.php
+++ xoops_version.php
@@ -34,8 +34,9 @@ $moduleDirNameUpper = \mb_strtoupper($moduleDirName);
 // ------------------- Information ------------------- /
 $modversion = [
     'version'             => '2.0.2',
-    'module_status'       => 'Beta1',
-    'release'             => '2024/04/23', // format: yyyy/mm/dd
+    'module_status'       => 'Stable',
+    'release'             => '2025/06/07', // format: yyyy/mm/dd
+    'release_date'        => '2025/06/07', // format: yyyy/mm/dd
     'name'                => _MI_WGTEAMS_NAME,
     'description'         => _MI_WGTEAMS_DESC,
     'author'              => 'Goffy - Wedega.com',
@@ -48,7 +49,6 @@ $modversion = [
     'help'                => 'page=help',
     'release_info'           => 'release_info',
     'release_file'        => \XOOPS_URL . '/modules/wgteams/docs/release_info file',
-    'release_date'        => '2024/03/07', // format: yyyy/mm/dd
     'manual'              => 'link to manual file',
     'manual_file'         => \XOOPS_URL . '/modules/wgteams/docs/install.txt',
     'min_php'             => '7.4',
